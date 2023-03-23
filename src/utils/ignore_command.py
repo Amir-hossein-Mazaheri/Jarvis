8cc7d343c9743a20398ef13e9335b21ea431a920
Amirhossein <uplayggboy@gmail.com>
2023-03-23 04:47:09 +0430

    refactor: made ignore protectors more fluent

--- src/utils/ignore_command.py
+++ src/utils/ignore_command.py
@@ -2,6 +2,7 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 from src.utils.is_admin import is_admin
+from src.utils.get_actions_keyboard import get_actions_keyboard
 
 
 async def ignore_command(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
@@ -9,6 +10,6 @@ async def ignore_command(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
     admin = await is_admin(user_id)
 
     if not admin:
-        await ctx.bot.send_message(chat_id=update.effective_chat.id, text="You don't have access.")
+        await ctx.bot.send_message(chat_id=update.effective_chat.id, text="You don't have access.", reply_markup=await get_actions_keyboard(update, ctx))
 
     return not admin
--- src/utils/ignore_user.py
+++ src/utils/ignore_user.py
@@ -3,6 +3,7 @@ from telegram.ext import ContextTypes
 
 from src.utils.is_user_registered import is_user_registered
 from src.constants.other import LAST_MESSAGE_KEY
+from src.utils.get_actions_keyboard import get_actions_keyboard
 
 
 async def ignore_user(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
@@ -10,7 +11,7 @@ async def ignore_user(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
     is_registered = await is_user_registered(user_id)
 
     if not is_registered:
-        sent_message = await ctx.bot.send_message(chat_id=update.effective_chat.id, text="Please first register via /register command")
+        sent_message = await ctx.bot.send_message(chat_id=update.effective_chat.id, text="Please first register", reply_markup=await get_actions_keyboard(update, ctx))
         ctx.user_data[LAST_MESSAGE_KEY] = sent_message.id
 
     return not is_registered
